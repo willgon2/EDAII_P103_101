22cd9e5a5b9d59277295936410f0646617618709
iagoiagoiagoiago <iago.fontanals01@estudiant.upf.edu>
2025-05-02 13:39:32 +0200

    principìos de hashmap

---
+++ src/hashmap.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+
+typedef struct HashMap {
+    int size;
+    int capacity;
+    int *keys;
+    int *values;
+} HashMap;
+
+HashMap *create_hashmap(int capacity) {
+    HashMap *map = malloc(sizeof(HashMap));
+    map->size = 0;
+    map->capacity = capacity;
+    map->keys = malloc(capacity * sizeof(int));
+    map->values = malloc(capacity * sizeof(int));
+    return map;
+}
---
+++ src/hashmap.h
@@ -0,0 +1,6 @@
+#ifndef HASHMAP_LIB_H
+#define HASHMAP_LIB_H
+//aaa
+
+
+#endif
